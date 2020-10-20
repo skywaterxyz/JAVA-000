e5889902bbd30cfa0cea3f808359d183c123d235
xutianshui <773138585@qq.com>
2020-10-21 01:19:20 +0800

    第一次课第二题首次提交

---
+++ Week_01/2/MyClassLoader.java
@@ -0,0 +1,60 @@
+import java.io.ByteArrayOutputStream;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+
+public class MyClassLoader extends ClassLoader {
+
+	public static void main(String[] args) throws ClassNotFoundException, InstantiationException, IllegalAccessException, NoSuchMethodException, SecurityException, IllegalArgumentException, InvocationTargetException {
+		MyClassLoader myClassLoader = new MyClassLoader();
+		Class<?> helloClass = myClassLoader.findClass("Hello", "./Hello.xlass");
+		
+		Object helloInstance  = helloClass.newInstance();
+		
+		Method method = helloClass.getMethod("hello");
+		method.invoke(helloInstance);		
+	}
+
+	protected Class<?> findClass(String className, String fileName) throws ClassNotFoundException {
+		
+		byte[] decodedBytes = getDecodedBytes(fileName);
+		if (decodedBytes == null) {
+			System.out.print("get decoded bytes fail.");
+			return null;
+		}
+		return defineClass(className, decodedBytes, 0, decodedBytes.length);
+	}
+	
+	private byte[] getDecodedBytes(String fileName) {
+		
+		InputStream in = null;
+		byte[] decodedBytes = null;
+		try {
+			in = new FileInputStream(fileName);
+			ByteArrayOutputStream out = new ByteArrayOutputStream();
+			int byteTmp;
+			try {
+				while((byteTmp = in.read()) != -1) {
+					out.write(255 - byteTmp);
+				}
+				decodedBytes = out.toByteArray();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		} finally {
+			if (in != null) {
+				try {
+					in.close();
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+			}
+		}
+		return decodedBytes;
+	}
+}
---
+++ Week_01/2/README.txt
@@ -0,0 +1,3 @@
+运行说明：
+	执行如下命令即可运行，程序将解析出Hello.xlass中的内容。
+	命令:java MyClassLoader
